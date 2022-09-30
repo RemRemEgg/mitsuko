cfb77d097be2c925bfb901d38875180bf18b4561
Remmy <remremegg@gmail.com>
2022-09-29 21:33:26 -0600

    Main

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,96 @@
+#![allow(warnings)]
+
+#[allow(warnings)]
 mod server;
 
 use std::fs;
 use server::*;
+use std::time::Instant;
 
 static DEBUG: bool = true;
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
+    /*    let contents = String::from("
+    //This is a comment
+    // And Another
+    #[remgine=false]
+    #[dead=true]
+
+    fn main() {
+        balls();
+
+        //Should this be called?
+        overthrow();
+    }
+
+    fn balls() {
+
+    }
+
+    // This is a bad function!!!
+    fn overthrow () {}");*/
     compile(contents);
 }
 
-
 fn compile(input: String) {
-    if DEBUG { println!("Received {}", input) };
-    let mut lines = input.split("\n").collect::<Vec<&str>>().iter().map(|s| String::from(*s)).collect::<Vec<String>>();
+    println!("> Compiling");
+    let mut s = Instant::now();
+    let mut lines = input.split("\n").collect::<Vec<&str>>().iter().map(|s| String::from(String::from(*s).trim())).collect::<Vec<String>>();
 
-    lines = trim_white_space(lines);
+    let (lines, rem) = trim_white_space(lines);
+    let t = lines.len() + rem;
+    println!("> Trimmed {} lines ({}, {}%) in {} ms", rem, t, rem * 100 / t, s.elapsed().as_millis());
 
     let mut pack = Datapack::new(lines);
 
-    print_lines(pack);
+    let (pack, status) = scan_pack(pack);
+
+    print_lines(&pack);
+
+    println!("> Compilition  {} lines ({}, {}%) in {} ms", rem, t, rem * 100 / t, s.elapsed().as_millis());
+}
+
+fn scan_pack(mut pack: Datapack) -> (Datapack, u8) {
+    'lines: loop {
+        if pack.lines.len() > 0 {
+            println!("Found EOF");
+            break 'lines;
+        }
+        let remove = scan_pack_line(pack.lines[0].to_string());
+        for r in 0..remove {
+            pack.lines.remove(r);
+        }
+    }
+    (pack, 0)
+}
+
+fn scan_pack_line(line: String) -> usize {
+    0
 }
 
 pub struct Datapack {
-    ln : i32,
-    remgine : bool,
-    opt_level : i32,
-    call : bool,
-    lines : Vec<String>,
-    functions : Vec<MCFunction>
+    ln: i32,
+    remgine: bool,
+    opt_level: i32,
+    call: bool,
+    lines: Vec<String>,
+    functions: Vec<MCFunction>,
 }
 
 impl Datapack {
-    fn new(lines : Vec<String>) -> Datapack {
+    fn new(lines: Vec<String>) -> Datapack {
         Datapack { ln: 0, remgine: true, opt_level: 0, call: false, lines, functions: vec![] }
     }
 }
 
 pub struct MCFunction {
-    lines : Vec<String>,
-    commands : Vec<String>
+    lines: Vec<String>,
+    commands: Vec<String>,
 }
 
 impl MCFunction {
     fn new() -> MCFunction {
-        MCFunction{ lines: vec![], commands: vec![] }
+        MCFunction { lines: vec![], commands: vec![] }
     }
 }
-
-/*
-// Comment
-#[var=val]
-cmd say Direct Command
-
-if (statement) {
-    commands
-}
-
-eif (statement) command
-
- */
--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,29 @@
 use crate::Datapack;
 
-pub fn print_lines(input : Datapack) {
+pub fn print_lines(input: &Datapack) {
     for (i, e) in input.lines.iter().enumerate() {
         println!("[{}] {}", i, e);
     }
 }
 
-pub fn trim_white_space(input : Vec<String>) -> Vec<String> {
+pub fn trim_white_space(input: Vec<String>) -> (Vec<String>, usize) {
     let mut out = Vec::new();
+    let mut c: usize = 0usize;
     for i in input {
-        if i.chars().next() != Some('\r') {
+        if !(i.is_empty() || i.starts_with("//")) {
             out.push(i);
+        } else {
+            c = c + 1usize;
         }
     }
-    out
+    (out, c)
+}
+
+pub fn warn(warning: &str) {
+    println!("[Warning] {}", warning);
+}
+
+pub fn error(error: &str) -> ! {
+    println!("[Error] {}", error);
+    panic!();
 }
